a4371d7b737565349c94476a216f74e3381ffd7e
Oscar <ohohurtado16@gmail.com>
2023-04-29 15:27:37 -0500

    Agregar archivos de proyecto.

---
+++ Lab_06/Lab_06/ListViewSource.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab_06
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewSource : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+
+        public ListViewSource()
+        {
+            InitializeComponent();
+            //ObservableCollection allows item to be added after ItemsSource
+            //is set and the YU will react to chhanges
+            EmployeeView.ItemsSource = employees;
+            employees.Add(new Employee { DisplayName = "Rob Finnerty" });
+            employees.Add(new Employee { DisplayName = "Bill Wrestler" });
+            employees.Add(new Employee { DisplayName = "Dr. Geri-Betch Hooper" });
+            employees.Add(new Employee { DisplayName = "Dr. Keith Joyce-Purdy" });
+            employees.Add(new Employee { DisplayName = "Sheri Spruce" });
+            employees.Add(new Employee { DisplayName = "Burt Indybrick" });
+
+        }
+    }
+}
